4c89cbe2a6e0d0e20d1cdc22041f8af5042b049e
Ko <64240632+GenKoKo@users.noreply.github.com>
2021-04-05 11:13:47 +0900

    Update app.py

--- app.py
+++ app.py
@@ -11,10 +11,10 @@ app = Flask(__name__,
         )
 app.secret_key = ''
 
-app.config["MYSQL_HOST"] = 'localhost'
-app.config["MYSQL_USER"] = 'root'
+app.config["MYSQL_HOST"] = ''
+app.config["MYSQL_USER"] = ''
 app.config["MYSQL_PASSWORD"] = ''
-app.config["MYSQL_DB"] = 'wk6'
+app.config["MYSQL_DB"] = ''
 
 mysql = MySQL(app)
 
